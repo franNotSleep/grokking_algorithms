9b489c1a2f6391a1871723852b38c3d9ec6d9f5a
Francisco Jose <121639060+franNotSleep@users.noreply.github.com>
2023-07-08 20:57:12 -0400

    Update 02_longest_common_substring: fix index out of range

--- 09_dynamic_programming/python/02_longest_common_substring.py
+++ 09_dynamic_programming/python/02_longest_common_substring.py
@@ -1,12 +1,11 @@
 dp_table_blue = ["b", "l", "u", "e"]
 dp_table_clues = ["c", "l", "u", "e", "s"]
-dp_table = [[0 for i in range(len(dp_table_blue))] for i in range(len(dp_table_clues))] # (5,4)
-print(dp_table)
+dp_table = [[0 for i in range(len(dp_table_clues))] for i in range(len(dp_table_blue))]
 
-for i in range(0, len(dp_table_blue)):
-    for j in range(0, len(dp_table_clues)):
-        if dp_table_clues[j] == dp_table_blue[i]:
-            dp_table[i][j] = dp_table[i-1][i-1] + 1
+for i in range(len(dp_table_blue)):
+    for j in range(len(dp_table_clues)):
+        if dp_table_blue[i] == dp_table_clues[j]:
+            dp_table[i][j] = dp_table[i-1][j-1] + 1
         else:
             dp_table[i][j] = 0
 
